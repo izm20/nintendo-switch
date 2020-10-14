d079257789b1d75bb10add3c6f24c8ee9a89b759
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-10-14 18:12:28 +0100

    Improve joystick and base shadows

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
-const win = { width: document.documentElement.clientWidth, height: document.documentElement.clientHeight };
-
-window.addEventListener('resize', function () {
-	win.width = document.documentElement.clientWidth;
-	win.height = document.documentElement.clientHeight;
-});
 const leftJoystick = document.querySelector('.left-joycon .joystick');
-const joy = { x: leftJoystick.getBoundingClientRect().left + 35.5, y: leftJoystick.getBoundingClientRect().top + 40 };
-const ORIGIN_JOY = joy;
+const rightJoystick = document.querySelector('.right-joycon .joystick');
+
+const CIR_OFFSET_X = 35.5;
+const CIR_OFFSET_Y = 40;
+const leftJoy = { x: leftJoystick.getBoundingClientRect().left + CIR_OFFSET_X, y: leftJoystick.getBoundingClientRect().top + CIR_OFFSET_Y };
+const LEFT_ORIGIN_JOY = leftJoy;
+const rightJoy = { x: rightJoystick.getBoundingClientRect().left + CIR_OFFSET_X, y: rightJoystick.getBoundingClientRect().top + CIR_OFFSET_Y };
+const RIGHT_ORIGIN_JOY = rightJoy;
 const mouse = { x: 0, y: 0 };
 let thumb = false;
 
-const mouseMove = (e) => {
+const mouseMove = (e, update) => {
 	mouse.x = e.clientX;
 	mouse.y = e.clientY;
 	requestAnimationFrame(update);
@@ -18,42 +18,76 @@ const mouseMove = (e) => {
 
 const addMouseMove = () => {
 	thumb = true;
-	leftJoystick.addEventListener('mousemove', mouseMove);
+	leftJoystick.addEventListener('mousemove', (e) => mouseMove(e, updateLeft));
+	rightJoystick.addEventListener('mousemove', (e) => mouseMove(e, updateRight));
 };
 
 leftJoystick.addEventListener('mousedown', addMouseMove);
+rightJoystick.addEventListener('mousedown', addMouseMove);
 
 leftJoystick.addEventListener('mouseup', () => {
 	thumb = false;
-	leftJoystick.removeEventListener('mousemove', mouseMove);
-	requestAnimationFrame(update);
+	leftJoystick.removeEventListener('mousemove', (e) => mouseMove(e, updateLeft));
+	requestAnimationFrame(updateLeft);
+});
+
+rightJoystick.addEventListener('mouseup', () => {
+	thumb = false;
+	rightJoystick.removeEventListener('mousemove', (e) => mouseMove(e, updateRight));
+	requestAnimationFrame(updateRight);
 });
 
 leftJoystick.addEventListener('mouseout', () => {
 	thumb = false;
-	leftJoystick.removeEventListener('mousemove', mouseMove);
-	requestAnimationFrame(update);
+	leftJoystick.removeEventListener('mousemove', (e) => mouseMove(e, updateLeft));
+	requestAnimationFrame(updateLeft);
+});
+
+rightJoystick.addEventListener('mouseout', () => {
+	thumb = false;
+	rightJoystick.removeEventListener('mousemove', (e) => mouseMove(e, updateRight));
+	requestAnimationFrame(updateRight);
 });
 
 function updateJoy () {
-	joy.x = leftJoystick.getBoundingClientRect().left + 35.5;
-	joy.y = leftJoystick.getBoundingClientRect().top + 40;
+	leftJoy.x = leftJoystick.getBoundingClientRect().left + CIR_OFFSET_X;
+	leftJoy.y = leftJoystick.getBoundingClientRect().top + CIR_OFFSET_Y;
+	rightJoy.x = rightJoystick.getBoundingClientRect().left + CIR_OFFSET_X;
+	rightJoy.y = rightJoystick.getBoundingClientRect().top + CIR_OFFSET_Y;
 }
 
-const RANGE = 3;
+const RANGE = 2;
+const ROTATE_RANGE = 2.5;
 
-function update () {
+function updateLeft () {
 	if (thumb) {
-		const offsetX = -(joy.x - mouse.x) / RANGE;
-		const offsetY = -(joy.y - mouse.y) / RANGE;
-		const xRotate = Math.abs(offsetX * 2);
-		const yRotate = Math.abs(offsetY * 2);
-		console.log('y:', yRotate, 'x:', xRotate);
-		leftJoystick.style.transform = `translateX(${offsetX}px) translateY(${offsetY}px) rotateY(${xRotate}deg) rotateX(${yRotate}deg) perspective(5000rem)`;
+		const offsetX = (leftJoy.x - mouse.x) / -RANGE;
+		const offsetY = (leftJoy.y - mouse.y) / -RANGE;
+		const rotateY = offsetX * ROTATE_RANGE;
+		const rotateX = offsetY * -ROTATE_RANGE;
+		console.log('y:', rotateX, 'x:', rotateY);
+		leftJoystick.style.transform = `translateX(${offsetX}px) translateY(${offsetY}px) rotateY(${rotateY}deg) rotateX(${rotateX}deg) perspective(5000rem)`;
+
 		updateJoy();
 	} else {
-		leftJoystick.style.transform = ``;
-		joy.x = ORIGIN_JOY.x;
-		joy.y = ORIGIN_JOY.y;
+		leftJoystick.style.transform = '';
+		leftJoy.x = LEFT_ORIGIN_JOY.x;
+		leftJoy.y = LEFT_ORIGIN_JOY.y;
+	}
+}
+
+function updateRight () {
+	if (thumb) {
+		const offsetX = -(rightJoy.x - mouse.x) / RANGE;
+		const offsetY = -(rightJoy.y - mouse.y) / RANGE;
+		const rotateY = offsetX * ROTATE_RANGE;
+		const rotateX = offsetY * -ROTATE_RANGE;
+		console.log('y:', rotateX, 'x:', rotateY);
+		rightJoystick.style.transform = `translateX(${offsetX}px) translateY(${offsetY}px) rotateY(${rotateY}deg) rotateX(${rotateX}deg) perspective(5000rem)`;
+		updateJoy();
+	} else {
+		rightJoystick.style.transform = '';
+		rightJoy.x = RIGHT_ORIGIN_JOY.x;
+		rightJoy.y = RIGHT_ORIGIN_JOY.y;
 	}
 }
