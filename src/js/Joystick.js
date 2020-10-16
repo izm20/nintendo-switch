5ea14bbc60945b700f394f17e5ecfe3a9a0b0c67
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-10-16 17:53:25 +0100

    Improve the move and added the interactive menu

--- src/js/Joystick.js
+++ src/js/Joystick.js
@@ -1,27 +1,47 @@
 export class Joystick {
 	constructor (element) {
 		this.element = element;
-		this.CIR_OFFSET_X = 35.5;
-		this.CIR_OFFSET_Y = 40;
+		this.CIR_OFFSET_X = 23;
+		this.CIR_OFFSET_Y = 25;
 		this.RANGE = 2;
 		this.ROTATE_RANGE = 2.5;
 		this.joyCoordinates = { x: this.element.getBoundingClientRect().left + this.CIR_OFFSET_X, y: this.element.getBoundingClientRect().top + this.CIR_OFFSET_Y };
 		this.ORIGIN_JOY_COORDINATES = this.joyCoordinates;
 		this.isActive = false;
+		this.countLeft = 0;
+		this.countRight = 0;
 	}
 
-	update (mouse, self) {
+	update (mouse, self, screen, moveScreen) {
 		if (self.isActive) {
 			const offsetX = (self.joyCoordinates.x - mouse.x) / -self.RANGE;
 			const offsetY = (self.joyCoordinates.y - mouse.y) / -self.RANGE;
 			const rotateY = offsetX * self.ROTATE_RANGE;
 			const rotateX = offsetY * -self.ROTATE_RANGE;
+
+			console.log(offsetX, offsetY);
 			self.element.style.transform = `translateX(${offsetX}px) translateY(${offsetY}px) rotateY(${rotateY}deg) rotateX(${rotateX}deg) perspective(5000rem)`;
 			self.updateCoordinates(self);
+
+			if (offsetX >= 5) {
+				if (self.countRight >= 20) {
+					moveScreen.right(screen);
+					self.countRight = 0;
+				}
+				self.countRight++;
+			} else if (offsetX <= -6) {
+				if (self.countLeft >= 20) {
+					moveScreen.left(screen);
+					self.countLeft = 0;
+				}
+				self.countLeft++;
+			}
 		} else {
 			self.element.style.transform = '';
 			self.joyCoordinates.x = self.ORIGIN_JOY_COORDINATES.x;
 			self.joyCoordinates.y = self.ORIGIN_JOY_COORDINATES.y;
+			self.countLeft = 0;
+			self.countRight = 0;
 		}
 	}
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,30 @@
 import { Joystick } from './Joystick';
+import videoMario from '../assets/video/Mario.mp4';
+import videoZelda from '../assets/video/Zelda.mp4';
+import video12 from '../assets/video/1-2.mp4';
+import videoArms from '../assets/video/Arms.mp4';
+import videoSnipper from '../assets/video/Snipperclip.mp4';
+
 
 const leftJoystick = new Joystick(document.querySelector('.left-joycon .joystick'));
 const rightJoystick = new Joystick(document.querySelector('.right-joycon .joystick'));
+addEvents(leftJoystick, mouseMove);
+addEvents(rightJoystick, mouseMove);
+
+const btnHome = document.querySelector('.right-joycon .button-home');
+const btnA = document.querySelector('.right-joycon .button-right');
+const btnB = document.querySelector('.right-joycon .button-down');
+
+const screen = document.querySelector('.screen');
+const video = document.querySelector('.video');
 
 const mouse = { x: 0, y: 0 };
 
-const mouseMove = function (e, joystick) {
+function mouseMove (e, joystick) {
 	mouse.x = e.clientX;
 	mouse.y = e.clientY;
-	requestAnimationFrame(() => joystick.update(mouse, joystick));
+	requestAnimationFrame(() => joystick.update(mouse, joystick, screen, { right: moveRight, left: moveLeft }));
 };
-
-addEvents(leftJoystick, mouseMove);
-addEvents(rightJoystick, mouseMove);
 
 function addMouseMove (mouseMove, joystick) {
 	joystick.isActive = true;
@@ -33,4 +45,97 @@ function removeMouseMove (mouseMove, joystick) {
 	joystick.isActive = false;
 	joystick.element.removeEventListener('mousemove', function (e) { mouseMove(e, joystick); });
 	requestAnimationFrame(() => joystick.update(mouse, joystick));
+}
+
+btnHome.addEventListener('click', (e) => {
+	video.pause();
+	video.src = '';
+	screen.classList.toggle('opacity');
+	screen.classList.remove('menu-2', 'menu-3', 'menu-4', 'menu-5');
+});
+
+const rightArrow = document.querySelector('.left-joycon .button-arrow-right');
+rightArrow.addEventListener('click', (e) => {
+	moveRight(screen);
+});
+
+const leftArrow = document.querySelector('.left-joycon .button-arrow-left');
+leftArrow.addEventListener('click', (e) => {
+	moveLeft(screen);
+});
+
+function moveRight (screen) {
+	if (screen.classList.contains('opacity')) {
+		if (!screen.classList.contains('menu-2')) {
+			screen.classList.add('menu-2');
+		} else {
+			if (!screen.classList.contains('menu-3')) {
+				screen.classList.add('menu-3');
+			} else {
+				if (!screen.classList.contains('menu-4')) {
+					screen.classList.add('menu-4');
+				} else {
+					if (!screen.classList.contains('menu-5')) {
+						screen.classList.add('menu-5');
+					}
+				}
+			}
+		}
+	}
+}
+
+function moveLeft (screen) {
+	if (screen.classList.contains('opacity')) {
+		if (screen.classList.contains('menu-5')) {
+			screen.classList.remove('menu-5');
+		} else {
+			if (screen.classList.contains('menu-4')) {
+				screen.classList.remove('menu-4');
+			} else {
+				if (screen.classList.contains('menu-3')) {
+					screen.classList.remove('menu-3');
+				} else {
+					if (screen.classList.contains('menu-2')) {
+						screen.classList.remove('menu-2');
+					}
+				}
+			}
+		}
+	}
+}
+
+btnA.addEventListener('click', (e) => {
+	if (screen.classList.length === 2) {
+		playVideo(videoZelda);
+	}
+	if (screen.classList.length === 3) {
+		playVideo(videoMario);
+	}
+	if (screen.classList.length === 4) {
+		playVideo(video12);
+	}
+	if (screen.classList.length === 5) {
+		playVideo(videoArms);
+	}
+	if (screen.classList.length === 6) {
+		playVideo(videoSnipper);
+	}
+});
+
+btnB.addEventListener('click', (e) => {
+	video.classList.remove('active');
+	setTimeout(() => {
+		video.pause();
+		video.src = '';
+	}, 1000);
+});
+
+function playVideo (srcVideo) {
+	video.pause();
+	video.src = srcVideo;
+	video.load();
+	setTimeout(() => {
+		video.play();
+	}, 1000);
+	video.classList.add('active');
 }
